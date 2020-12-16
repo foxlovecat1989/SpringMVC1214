a71d619ac480c17e301d14f742d2bcc7980147bd
foxlo <foxlo@DESKTOP-TRH15VU>
2020-12-17 01:43:14 +0800

    SpringMVC - use layout

---
+++ src/main/java/com/spring/mvc/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dept")
+public class DeptController {
+    
+    @RequestMapping("/")
+    public String read(){
+        return "dept_page";
+    }
+}
